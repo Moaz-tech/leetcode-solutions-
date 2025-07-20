7471650f4142165117b4dbd7c023257ee02cfd26
Moaz-tech <moaznadeem73333@gmail.com>
2025-07-20 17:44:02 +0500

    Update TwoSum.cpp

--- Easy/TwoSum.cpp
+++ Easy/TwoSum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < 3; i++) {                        //So Basically, I have initiated two loops here. 
+        for(int i = 0; i < nums.size() - 1; i++) {                        //So Basically, I have initiated two loops here. 
             for(int j = i + 1; j < nums.size(); j++) {      //nums.size() means it will give me the size of the array or vector and hence I can tell the J variable not to go beyond the size of the array or vector.
                 if(nums[i] + nums[j] == target) {
                     return {i, j};
